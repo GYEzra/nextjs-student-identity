d424835babc205ac484ff9b0c3c2f1988cd06c40
camtu470 <truongtu09821@gmail.com>
2024-12-20 15:05:28 +0700

    bổ sung code nft detail

--- src/components/ui/navbar/index.tsx
+++ src/components/ui/navbar/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -6,69 +6,69 @@ import Walletbar from "../wallet-bar";
 import { useAccount, useNetwork } from "@/components/hooks/web3";
 
 const navigation = [
-    { label: "Create NFT", href: "/nft/create", activeClass: "" },
-    { label: "Edit Profile", href: "/profile/edit", activeClass: "" },
-    { label: "Detail NFT", href: "/nft/nft-detail", activeClass: "" },
-    { label: "Blog", href: "/blog", activeClass: "" },
-    { label: "About", href: "/about", activeClass: "" },
+  { label: "Create NFT", href: "/nft/create", activeClass: "" },
+  { label: "Edit Profile", href: "/profile/edit", activeClass: "" },
+  { label: "Detail NFT", href: "/nft/detail", activeClass: "" },
+  { label: "Blog", href: "/blog", activeClass: "" },
+  { label: "About", href: "/about", activeClass: "" },
 ];
 
 const Navbar = () => {
-    return (
-        <div className="navbar bg-black">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="white"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16"
-                            />
-                        </svg>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-                    >
-                        {navigation.map((item) => (
-                            <li key={item.label}>
-                                <Link href={item.href}>
-                                    <span className="text-base text-black">{item.label}</span>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <Link href="/">
-                    <span className="btn btn-ghost text-xl special-text">FINTECH</span>
+  return (
+    <div className="navbar bg-black">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {navigation.map((item) => (
+              <li key={item.label}>
+                <Link href={item.href}>
+                  <span className="text-base text-black">{item.label}</span>
                 </Link>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {navigation.map((item) => (
-                        <li key={item.label}>
-                            <Link href={item.href}>
-                                <span className="text-neutral-400 text-base hover:text-neutral-200">
-                                    {item.label}
-                                </span>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <Walletbar />
-            </div>
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+        <Link href="/">
+          <span className="btn btn-ghost text-xl special-text">FINTECH</span>
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {navigation.map((item) => (
+            <li key={item.label}>
+              <Link href={item.href}>
+                <span className="text-neutral-400 text-base hover:text-neutral-200">
+                  {item.label}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <Walletbar />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

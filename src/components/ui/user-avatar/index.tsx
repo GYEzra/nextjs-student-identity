aa8e67acb983fd8fa71f3dcf73c30f0feff344ba
“Duy <pdk202016@gmail.com>
2025-01-17 20:57:52 +0700

    fix error

--- src/components/ui/user-avatar/index.tsx
+++ src/components/ui/user-avatar/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import AvatarPlaceholder from "../avatar-placeholder";
 
 const UserAvatar = ({ image, name }: { image?: string; name: string }) => {
-  if (image) return <Image loader={imageLoader} src={image} alt="Avatar User" width={1} height={1} className="rounded-full object-cover w-36 h-36 mx-auto" />;
+  if (image) return <Image loader={imageLoader} src={image} alt="Avatar User" width={1} height={1} className="rounded-full object-cover w-36 h-36 mx-auto" unoptimized />;
   return <AvatarPlaceholder placeholder={name[0]} />;
 };
 

a84220da1766f23f507462dfce0791ca37d75109
“Duy <pdk202016@gmail.com>
2025-02-11 14:22:14 +0700

    fix error

--- src/hooks/web3/useListedNfts.ts
+++ src/hooks/web3/useListedNfts.ts
@@ -15,35 +15,42 @@ export type UseListedNftsHook = ReturnType<ListedNftsHookFactory>;
 
 export const hookFactory: ListedNftsHookFactory =
   ({ contract }) =>
-  () => {
-    const { data, ...swr } = useSWR("web3/useListedNfts", async () => {
-      const queryParams = {
-        "filter[isListed]": true,
+    () => {
+      const { data, ...swr } = useSWR("web3/useListedNfts", async () => {
+        const queryParams = {
+          "filter[isListed]": true,
+        };
+        const { data } = await getNfts(queryParams);
+        return data;
+      }, {
+        revalidateOnFocus: false,
+        revalidateOnMount: false,
+        revalidateOnReconnect: false,
+        refreshWhenOffline: false,
+        refreshWhenHidden: false,
+        refreshInterval: 1
+      });
+
+      const buyNft = useCallback(
+        async (tokenId: number, price: number) => {
+          try {
+            const convertValue = ethers.parseEther(price.toString());
+            const promise = contract!.buy(tokenId, { value: BigInt(convertValue) }).then((tx) => tx.wait());
+
+            await toast.promise(promise, {
+              pending: "Processing the transaction",
+              success: "Buy successfully",
+            });
+          } catch (error: any) {
+            toast.error(error.shortMessage || error.message);
+          }
+        },
+        [contract]
+      );
+
+      return {
+        ...swr,
+        data: data || [],
+        buyNft,
       };
-      const { data } = await getNfts(queryParams);
-      return data;
-    });
-
-    const buyNft = useCallback(
-      async (tokenId: number, price: number) => {
-        try {
-          const convertValue = ethers.parseEther(price.toString());
-          const promise = contract!.buy(tokenId, { value: BigInt(convertValue) }).then((tx) => tx.wait());
-
-          await toast.promise(promise, {
-            pending: "Processing the transaction",
-            success: "Buy successfully",
-          });
-        } catch (error: any) {
-          toast.error(error.shortMessage || error.message);
-        }
-      },
-      [contract]
-    );
-
-    return {
-      ...swr,
-      data: data || [],
-      buyNft,
     };
-  };
